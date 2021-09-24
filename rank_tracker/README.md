174df1f788fd9ff27a2d55cffb0a388090b64af2
Nuel <32788259+Hzzkygcs@users.noreply.github.com>
2021-09-24 13:59:29 +0700

    .

---
+++ rank_tracker/README.md
@@ -0,0 +1,22 @@
+# Rank Tracker
+
+
+
+Penasaran siapa saja yang men-vote anda? Namun lelah kalau harus ngecek satu-per-satu ranking orang-orang? Mungkin anda sudah berada pada repo yang tepat :D
+
+
+
+Rank tracker merupakan sebuah script yang ditulis dalam python. Gunanya adalah supaya teman-teman bisa lebih mudah dalam mengecek rank tanpa harus repot-repot mengecek halaman github satu-per-satu. Caranya tinggal menjalankan script main.py ini di laptop teman-teman. Setelah itu, jika tidak ada masalah (misalnya masalah koneksi, dsb), maka teman-teman akan melihat file yang namanya kira-kira seperti ini: `update 24-09-2021 13-49.txt`
+Tinggal dibuka, dan disitu akan ada daftar akun-akun github beserta orang-orang yang men-vote orang tersebut.
+
+Jika ada masalah koneksi saat menjalankan, mungkin perlu mencoba memakai VPN. Misalnya Telkomsel kadang sulit untuk mengakses github page.
+
+Jika ada masalah karena library not found, cukup jalankan perintah ini pada command prompt (windows):
+
+```
+pip install requests
+```
+
+
+
+Selamat mencoba :D
---
+++ rank_tracker/main.py
@@ -0,0 +1,264 @@
+import requests
+import warnings
+import threading
+import time, os, pickle
+from time import gmtime, strftime
+from requests.status_codes import codes
+
+
+"""
+You might want to check whether your internet connection can access <username>.github.io/os212/
+with a reasonable time. If you encounter a connection error from this script, you need
+to re-check your connection. You might also want to use VPN (like warp VPN, etc) to speed it up.
+
+Misalnya, Telkomsel sepertinya bermasalah buat akses username.github.io, jadi mungkin perlu VPN buat
+pengguna telkomsel :' :(
+"""
+
+
+
+# menghilangkan warning
+warnings.filterwarnings("ignore")
+
+
+
+
+def main():
+    result = get_voted_multithreading()
+
+    file_update = open(strftime("update %d-%m-%Y %H-%M.txt"), 'w')
+
+    print()
+    print()
+
+    for voted_acc in result:
+        weeks = sorted(result[voted_acc].keys(), reverse=True)
+        total_vote = 0
+
+        for week in weeks:
+            total_vote += len(result[voted_acc][week])
+        
+        print(f"{voted_acc} ({total_vote}):")
+        print(f"{voted_acc} ({total_vote}):", file=file_update)
+        
+        for week in weeks:
+            week_vote_count = len(result[voted_acc][week])
+            print_msg = f"    W{week:02d} ({week_vote_count}) = {', '.join(result[voted_acc][week])}"
+            print(print_msg)
+            print(print_msg, file=file_update)
+        print()
+        print(file=file_update)
+
+
+    print()
+    print()
+    print("DONE")
+
+
+
+
+
+
+
+# credits: https://stackoverflow.com/a/6894023/7069108
+class ThreadWithReturnValue(threading.Thread):
+    def __init__(self, group=None, target=None, name=None,
+                 args=(), kwargs={}, Verbose=None):
+        threading.Thread.__init__(self, group, target, name, args, kwargs)
+        self._return = None
+        
+    def run(self):
+        if self._target is not None:
+            self._return = self._target(*self._args,
+                                                **self._kwargs)
+    def join(self, *args):
+        threading.Thread.join(self, *args)
+        return self._return
+
+
+
+
+
+with open("os_acc_list.txt", 'r') as f:
+    account_list = f.read().strip().split("\n")
+
+
+
+FIVETEEN_MINUTES = 60*5
+
+# Membuka request cache asalkan tidak lebih dari 5 menit yang lalu
+if (os.path.isfile("cache") and (time.time() - os.path.getmtime("cache")) < FIVETEEN_MINUTES):
+    with open("cache", 'rb') as f:
+        request_cache = pickle.load(f)
+        reused_cache = request_cache.copy()
+    did_reuse_cache = True
+else:
+    request_cache = {}
+    did_reuse_cache = False
+
+
+def save_request_cache():
+    if not did_reuse_cache or len(request_cache) > len(reused_cache):
+        with open("cache", 'wb') as f:
+            pickle.dump(request_cache, f)
+
+def make_a_request(url, verify, timeout):
+    if url in request_cache:
+        return request_cache[url]
+
+    req = requests.get(url, verify=verify, timeout=timeout)
+    request_cache[url] = req
+    return req
+
+
+
+
+
+# key: the voted person
+# value:
+#   Another dictionary:
+#       key:   the number of week (int)
+#       value: a list of the voter
+voter = {}
+
+
+def get_voted():
+
+    # key: the voted account. value: another dictionary <week:int, voters:list>
+    ret = {}
+    
+    for acc in account_list:
+        votes = get_votes(acc)
+
+        for week in votes:
+            for voted_acc in votes[week]:
+                if voted_acc not in ret:
+                    ret[voted_acc] = {}
+
+                if week not in ret[voted_acc]:
+                    ret[voted_acc][week] = []
+
+                ret[voted_acc][week].append(acc)
+    return ret
+
+
+
+def get_voted_multithreading():
+    # key: the voted account. value: another dictionary <week:int, voters:list>
+    ret = {}
+    MAX_NUM_OF_REQ_AT_A_TIME = 17
+
+    threads = {}  # key: the voter account, value: the thread
+
+    counter = 0
+    for acc in account_list:
+        thread = ThreadWithReturnValue(target=get_votes, args=(acc,))
+        thread.start()
+        threads[acc] = thread
+        
+        counter += 1
+        if counter % MAX_NUM_OF_REQ_AT_A_TIME == 0:
+            print(counter)
+
+            try:
+                process_the_threads(ret, threads)
+            except Exception as e:
+                save_request_cache()
+                raise e
+            
+            time.sleep(1)
+
+            if counter % (2*MAX_NUM_OF_REQ_AT_A_TIME) == 0:
+                save_request_cache()
+            
+    process_the_threads(ret, threads)
+
+    return ret
+
+
+def process_the_threads(return_dict, threads_dict):
+    ret = return_dict
+    
+    for acc, thread in threads_dict.items():
+        print(f"getting {acc}")
+
+        votes = thread.join()
+        for i in range(2):  # up to 2 times retry
+            if (votes is not None):
+                break
+            votes = get_votes(acc)
+        
+        # print(acc, thread, votes)
+
+        for week in votes:
+            for voted_acc in votes[week]:
+                voted_acc = voted_acc.lower()
+                
+                if voted_acc not in ret:
+                    ret[voted_acc] = {}
+
+                if week not in ret[voted_acc]:
+                    ret[voted_acc][week] = []
+
+                ret[voted_acc][week].append(acc)
+                
+    threads_dict.clear()
+
+      
+
+def get_votes(voter_account, max_try_count=4, connection_time_out=10):
+    # returns a dict of <int, list>
+    # where the key is the week, and the list
+    # is a list of the voted people
+
+    url = f"http://{voter_account}.github.io/os212/TXT/myrank.txt"
+    allowed_status = (codes.OK, codes.NOT_FOUND)
+
+    req = None
+    for i in range(max_try_count):
+        try:
+            if (req := make_a_request(url, False, connection_time_out)
+                ).status_code in allowed_status:
+                break
+        except requests.exceptions.ReadTimeout:
+            pass
+    else:
+        msg = ""
+        if req is not None:
+            msg = f"req status code: {req.status_code}"
+        raise ConnectionError(msg)
+
+    if req.status_code == codes.NOT_FOUND:
+        return {}
+
+    text = req.text
+    lines = text.split("\n")
+    ret = {}
+
+    for line in lines:
+        try:
+            line = line.strip()
+            
+            if line.startswith("#"):
+                continue
+            if (line.lower().startswith("zczc")):
+                split_line = line.split()
+                
+                week_num = int(split_line[1][1:])
+                if week_num not in ret:
+                    ret[week_num] = []
+
+                voted = split_line[2]
+                ret[week_num].append(voted)
+        except Exception as e:
+            print()
+            print("------------ ERROR ------------")
+            print(url)
+            print(line)
+            print("-------------------------------")
+            print()
+    return ret
+
+
+
+main()
